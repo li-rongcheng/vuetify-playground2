dc5f975e0aee8ec035244fab462af1479c3dbe24
Ron <li.rongcheng@gmail.com>
2020-09-17 17:19:43 +1000

    move some content into components

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,29 +1,8 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
+      <v-col cols="12"><page-logo /></v-col>
+      <v-col class="mb-4"><welcome-info /></v-col>
       <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold mb-3">
           What's next?
@@ -83,9 +62,12 @@
 
 <script lang="ts">
 import Vue from "vue";
+import PageLogo from "@/components/PageLogo.vue";
+import WelcomeInfo from "@/components/WelcomeInfo.vue";
 
 export default Vue.extend({
   name: "HelloWorld",
+  components: { PageLogo, WelcomeInfo },
 
   data: () => ({
     ecosystem: [
---
+++ src/components/WelcomeInfo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1 class="display-2 font-weight-bold mb-3">
+      Welcome to Vuetify
+    </h1>
+
+    <p class="subheading font-weight-regular">
+      For help and collaboration with other Vuetify developers,
+      <br />please join our online
+      <a href="https://community.vuetifyjs.com" target="_blank"
+        >Discord Community</a
+      >
+    </p>
+  </div>
+</template>
