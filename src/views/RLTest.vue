eef562dddbf127d9bf5678ab2854499b76e71e53
Ron <li.rongcheng@gmail.com>
2020-09-17 16:51:28 +1000

    add icon demo

--- src/views/RLTest.vue
+++ src/views/RLTest.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
     <h1>RL test works</h1>
+    <hr />
+    <v-icon>mdi-open-in-new</v-icon>
+    <v-icon>mdi-ab-testing</v-icon>
+    <v-icon>mdi-account-box</v-icon>
+    <v-icon>mdi-snowflake-off</v-icon>
   </div>
 </template>
