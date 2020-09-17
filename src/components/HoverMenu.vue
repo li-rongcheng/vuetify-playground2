bf4ae466efa7720e3054028642be0ac4c77f47b8
Ron <li.rongcheng@gmail.com>
2020-09-17 22:22:45 +1000

    add demo for hover menu

---
+++ src/components/HoverMenu.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="text-center">
+    <v-menu open-on-hover bottom offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark v-bind="attrs" v-on="on">
+          Hover Menu
+        </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="listClicked(item)"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data: () => ({
+    items: [
+      { title: "item 1" },
+      { title: "item 2" },
+      { title: "item 3" },
+      { title: "item 4" },
+    ],
+  }),
+  methods: {
+    listClicked: (item: { title: string }) => {
+      console.log("listClicked() called, title:", item.title);
+    },
+  },
+});
+</script>
--- src/views/RLTest.vue
+++ src/views/RLTest.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
-    <h1>RL test works</h1>
+    <h1>RL test page</h1>
     <hr />
     <v-icon>mdi-open-in-new</v-icon>
     <v-icon>mdi-ab-testing</v-icon>
     <v-icon>mdi-account-box</v-icon>
     <v-icon>mdi-snowflake-off</v-icon>
+    <hr />
+    <hover-menu />
   </div>
 </template>
+<script lang="ts">
+import HoverMenu from "@/components/HoverMenu.vue";
+import Vue from "vue";
+export default Vue.extend({
+  components: { HoverMenu },
+});
+</script>
