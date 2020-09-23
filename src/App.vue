a89f633be9205e86a24d769d42ab85c87f684190
Ron <li.rongcheng@gmail.com>
2020-09-24 02:44:23 +1000

    move toolbar logos to ToolbarLogo.vue

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,12 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      <router-link to="/">
-        <div class="d-flex align-center">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-            transition="scale-transition"
-            width="40"
-          />
-
-          <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-            width="100"
-          />
-        </div>
-      </router-link>
+      <toolbar-logo />
 
       <v-spacer></v-spacer>
 
       <v-btn text router to="about">
         <span class="mr-2">About</span>
-        <!-- <v-icon>mdi-open-in-new</v-icon> -->
       </v-btn>
 
       <v-btn text router to="rltest">
@@ -43,12 +22,11 @@
 
 <script lang="ts">
 import Vue from "vue";
+import ToolbarLogo from "@/components/ToolbarLogo.vue";
 
 export default Vue.extend({
   name: "App",
-
-  components: {},
-
+  components: { ToolbarLogo },
   data: () => ({
     //
   }),
